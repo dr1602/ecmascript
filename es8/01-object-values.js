0b6d23ec2fd5bf68dbd379f11388c0c0612f511b
dr1602 <109833006+dr1602@users.noreply.github.com>
2022-12-30 01:29:38 -0600

    EcmaScript 8 Updates
    EcmaScript 8 Updates with functions such as entries, values & keys

---
+++ es8/01-object-values.js
@@ -0,0 +1,8 @@
+// propiedades enumerables de un objeto
+
+const countries = {MX: 'México', CO: 'Colombia', CL: 'Chile', PE: 'Peru'};
+console.log(Object.values(countries));
+
+
+// nos da un array de strings, así podemos hacer transformaciones de información
+// para aplicar métodos de map, feature, review, etc.
---
+++ es8/02-object-keys.js
@@ -0,0 +1,4 @@
+const countries = {MX: 'México', CO: 'Colombia', CL: 'Chile', PE: 'Peru'};
+console.log(Object.keys(countries));
+
+//[ 'MX', 'CO', 'CL', 'PE' ]
